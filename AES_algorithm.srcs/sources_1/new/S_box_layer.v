7a307d70cea7b24983559307a8a448cd944ae100
ukalla1 <32079346+ukalla1@users.noreply.github.com>
2019-03-31 01:55:26 -0400

    created an empty vivado project and created the s box file necessary for the first layers lut.

---
+++ AES_algorithm.srcs/sources_1/new/S_box_layer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2019 01:15:41 AM
+// Design Name: 
+// Module Name: S_box_layer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module S_box_layer(
+
+    );
+endmodule
